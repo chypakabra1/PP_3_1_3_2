8f46ae72f4b2dfdfb6ed227741c8386b04c7b4fc
chypakabra1 <157878299+chypakabra1@users.noreply.github.com>
2024-06-17 11:22:05 +0300

    Add files via upload

---
+++ PP_3_1_3_Boot_Security/src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImp.java
@@ -0,0 +1,53 @@
+package ru.kata.spring.boot_security.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.kata.spring.boot_security.demo.dao.UserDao;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.repositories.UserRepository;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+public class UserServiceImp implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImp(UserRepository userRepository) {
+
+        this.userRepository = userRepository;
+
+    }
+
+    @Override
+    public List<User> index() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User show(Long id) {
+        return userRepository.getById(id);
+    }
+
+    @Transactional
+    public void save(User user) {
+        userRepository.save(user);
+    }
+
+    @Transactional
+    @Override
+    public void update(User user, Long id) {
+        /*userRepository.updateUser(user, id);*/
+        user = userRepository.getById(id);
+        user.setName(user.getName());
+    }
+
+    @Transactional
+    @Override
+    public void delete(Long id) {
+        userRepository.deleteById(id);
+    }
+}
